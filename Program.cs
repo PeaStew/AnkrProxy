6df1e141da8427acd0f216358db46775d3408178
PeaStew <peter.gavin.stewart@gmail.com>
2022-06-17 14:00:50 +0100

    reverted to .net 5 type program

--- Program.cs
+++ Program.cs
@@ -1,94 +1,24 @@
-using System.Net;
-using Yarp.ReverseProxy.Configuration;
 
-var builder = WebApplication.CreateBuilder(args);
-var certPath = builder.Configuration.GetSection("Kestrel").GetSection("Endpoints").GetSection("HttpsInlineCertFile").GetSection("Certificate").GetValue<string>("Path");
-var certKey = builder.Configuration.GetSection("Kestrel").GetSection("Endpoints").GetSection("HttpsInlineCertFile").GetSection("Certificate").GetValue<string>("KeyPath");
-
-builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-
-builder.WebHost.UseKestrel(so =>
+namespace AnkrProxy
 {
-    so.Limits.MaxConcurrentConnections = 100000;
-    so.Limits.MaxConcurrentUpgradedConnections = 100000;
-    so.Limits.MaxRequestBodySize = 252428800;
-    
-    so.Listen(IPAddress.Loopback, 443,
-        listenOptions =>
-        {
-            listenOptions.UseHttps(certPath,
-                certKey);
-        });
-
-});
-
-var routes = new[]
-{
-    new RouteConfig()
+    /// <summary>
+    /// Class that contains the entrypoint for the Reverse Proxy sample app.
+    /// </summary>
+    public class Program
     {
-        RouteId = "route" + Random.Shared.Next(), // Forces a new route id each time GetRoutes is called.
-        ClusterId = "tron-mainnet-archive-rpc",
-        Match = new RouteMatch
+        /// <summary>
+        /// Entrypoint of the application.
+        /// </summary>
+        public static void Main(string[] args)
         {
-            Path = "/wallet/{**catch-all}"
+            CreateHostBuilder(args).Build().Run();
         }
-    },
-    new RouteConfig()
-    {
-        RouteId = "route" + Random.Shared.Next(), // Forces a new route id each time GetRoutes is called.
-        ClusterId = "tron-mainnet-archive-solidity-rpc",
-        Match = new RouteMatch
-        {
-            // Path or Hosts are required for each route. This catch-all pattern matches all request paths.
-            Path = "/walletsolidity/{**catch-all}"
-        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
-};
-
-var clusters = new[]
-{
-    new ClusterConfig()
-    {
-        ClusterId = "tron-mainnet-archive-rpc",
-        SessionAffinity = new SessionAffinityConfig { Enabled = true, Policy = "Cookie", AffinityKeyName = ".Yarp.ReverseProxy.Affinity" },
-        Destinations = new Dictionary<string, DestinationConfig>(StringComparer.OrdinalIgnoreCase)
-        {
-            { "node1", new DestinationConfig() { Address = "http://localhost:22311/" } },
-        }
-    },
-    new ClusterConfig()
-    {
-        ClusterId = "tron-mainnet-archive-solidity-rpc",
-        SessionAffinity = new SessionAffinityConfig { Enabled = true, Policy = "Cookie", AffinityKeyName = ".Yarp.ReverseProxy.Affinity" },
-        Destinations = new Dictionary<string, DestinationConfig>(StringComparer.OrdinalIgnoreCase)
-        {
-            { "node1", new DestinationConfig() { Address = "http://localhost:22312/" } }
-        }
-    }
-};
-
-var app = builder.Build();
-
-app.MapReverseProxy();
-app.UseRouting();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.Map("/update", context =>
-    {
-        context.RequestServices.GetRequiredService<InMemoryConfigProvider>().Update(routes, clusters);
-        return Task.CompletedTask;
-    });
-    // We can customize the proxy pipeline and add/remove/replace steps
-    endpoints.MapReverseProxy(proxyPipeline =>
-    {
-        // Use a custom proxy middleware, defined below
-        //proxyPipeline.Use(MyCustomProxyStep);
-        // Don't forget to include these two middleware when you make a custom proxy pipeline (if you need them).
-        proxyPipeline.UseSessionAffinity();
-        proxyPipeline.UseLoadBalancing();
-    });
-});
-app.UseHttpsRedirection();
-app.Run();
-
+}
